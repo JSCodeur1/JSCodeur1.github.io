8f46c01f8d0c0ca0441b4ea3f312893d37411c36
JSCodeur <138124682+JSCodeur1@users.noreply.github.com>
2024-05-21 21:59:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,253 @@
+/* ------------------ level ------------------ */
+
+level = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,0,0,0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,2,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,2,0,0,0,0,1,1,1,1,1,1,1,2,2,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,2,2,0,0,0,0,2,2,0,0,0,0,1,1,1,1,0,0,0,1,2,2,2,0,0,2,0,0,0,0,0,0,0,0,2,2,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+]    //                                                                       ↑
+
+/* ---------------- fonctions ---------------- */
+function orienter (n) {
+    deg = n;
+    joueur.style.transform = 'rotate('+deg+'deg)';
+}
+
+function monter (n) {
+    y -= n;
+    joueur.style.top = y.toString() + "px";
+}
+function sauter () {
+    if ((y <= bas && h < -15) || MORT) {
+        y = bas;
+        joueur.style.top = y.toString() + "px";
+        orienter(0);
+        J=false;
+        vide(true);
+        clearInterval(interval);
+        return 0;
+    } 
+    monter(h);
+    h -= 1;
+    deg += 180/31;
+    orienter(deg); 
+}
+
+function grid (left, top, width, height, opa, pique, id) {
+    d = document.createElement('div');
+    d.id = id;
+    body.appendChild(d);
+    if (pique) {
+        d.style.background = "url(file:///D:/Bureauloulous/maman/ief/costume1.svg)"
+        top += 2;
+    } else{
+        d.style.backgroundColor = "black";
+    }
+    d.style.opacity = opa;
+    d.style.top = (top).toString() + "px";
+    d.style.left = left.toString() + "px";
+    d.style.width = width.toString() + "px";
+    d.style.height = height.toString() + "px";
+    return d;
+}
+
+function jump () {
+    J=true;
+    h=15;
+    interval = setInterval(sauter, 1000/60);
+}
+
+function vraiY (fy) {
+    fy = 700-(fy);
+    fy = (fy-(fy%50))/50;
+    return fy;
+}
+
+function mort (lev, yy) {
+    sauv=yy;
+    lev += 4;
+    yy = vraiY(yy-25);
+    if (yy != 0) {
+        if (level[(9-yy)][lev-1] === 1 && yy != vraiY(bas)) {
+            bas = y-(y%50)+50;
+            y=bas
+            joueur.style.top = bas + "px";
+            h = -100;
+            orienter(0);
+            tomb = false;
+        }
+    } else if (tomb === true) {
+        console.log(yy + " " + lev);
+        if (level[9][lev-1] === 1) {
+            console.log("detect");
+            bas = y-(y%50)+50;
+            y=bas
+            joueur.style.top = bas + "px";
+            h = -100;
+            orienter(0);
+            tomb = false;
+        }
+    }
+    yy = vraiY (sauv);
+    if (level[8-yy][lev] === 1 || level[8-yy][lev] === 2) {
+        MORT = true;
+    }
+}
+
+
+
+function vide (ee) {
+    if (y === bas) {
+        yy = vraiY(y-25);
+        if (level[(9-yy)][lev+3] === 0) {
+            console.log("vide");
+            tomber(ee);
+        } else {
+            tomb = false;
+        }
+    }
+}
+
+function tomber (ee) {
+    tomb = true;
+    if (!ee) {
+        h = 0;
+    }
+    b = setInterval(tombb, 85/3);
+}
+
+function tombb () {
+    if (tomb){
+        monter(h);
+        deg += 180/31;
+        orienter(deg);
+        mort (lev, y);
+        if (!tomb) {
+            deg = 0;
+            orienter(deg);
+        }
+        h -= 0.5;
+    }
+}
+
+function rejouer () {
+    window.location.href = window.location.href;
+}
+
+
+/* ----------------- projet ----------------- */
+
+console.log("Geometry Dash !");
+
+const body = document.querySelector('body');
+const tailleY = window.innerHeight;
+const tailleX = window.innerWidth;
+const joueur = document.getElementById('player');
+const html = document.querySelector('html');
+var y = 700,
+    tomb=false,
+    J=false,
+    sauv,
+    MORT = false,
+    bas = 700,
+    lev=0,
+    gameX=0,
+    h,
+    deg=0,
+    hh,
+    k,
+    div,
+    game,
+    canvas;
+
+html.onclick = function () {
+    if (y === bas && !MORT && !tomb) {
+        jump();
+    }
+}
+
+//document.querySelector('html').style.backgroundSize = taille.toString() + 'px';
+joueur.style.top = y.toString() + "px";
+joueur.style.left = "100px";
+
+document.onkeydown = function(e) {
+    k = e.keyCode;
+    if ((k === 32 || k === 38 || k === 87) && y === bas && !MORT && !tomb) {
+        jump();
+    } else if (k === 65) {
+        /* ---------- debug ----------- */
+    }
+}
+
+game=[];
+level.forEach(function(item, index, array) {
+    div = [];
+    var b=50;
+    for (i=0; i<37; i++) {
+        if (item[i] === 1) {
+            div.push(grid(b, 308+(index*50),48,48, 1, false, "grid"))
+        } else  if (item[i] === 2) {
+            div.push(grid(b, 308+(index*50),48,48, 1, true, "pique"));
+        } else {
+            div.push(grid(b, 308+(index*50),48,48, 0, false, "grid"));
+        }
+        b += 50;
+    }
+    game.push(div);
+});
+
+
+a = setInterval(function(){
+    game.forEach(function(div, index, array) {
+        eff=true;
+        for (i=1; i<div.length; i++) {
+            if (MORT) {
+                document.getElementById("rejouer").style.visibility = "visible";
+                clearInterval(a);
+            }
+            d = div[i];
+            left = d.style.left;
+            left = Math.round(left.substring(0,left.length-2));
+            left -= 10;
+            gameX+=10;
+            d.style.left = left + "px";
+            if (left===-50 && eff) {
+                eff=false;
+                d.remove();
+                if (index === 0) {
+                    lev++;
+                }
+                if (gameX<3340){
+                    if (level[index][lev+36] === 1){
+                        div.push(grid(1770, 308+index*50,48,48,1, false, "grid"));
+                    } else if (level[index][lev+36] === 2){
+                        div.push(grid(1770, 308+index*50,48,48,1, true, "pique"));
+                    } else {
+                        div.push(grid(1770, 308+index*50,48,48,0, false, "grid"));
+                    }
+                } else {
+                    if (level[index][lev+36] === 1){
+                        div.push(grid(1760, 308+index*50,48,48,1, false, "grid"));
+                    } else if (level[index][lev+36] === 2){
+                        div.push(grid(1770, 308+index*50,48,48,1, true, "pique"));
+                    } else {
+                        div.push(grid(1760, 308+index*50,48,48,0, false, "grid"));
+                    }
+                }
+            }
+        }})
+    eff=true;
+    mort(lev, y);
+    vide(false);
+    if (!tomb) {
+        clearInterval(b);
+    }
+}, 75/3) //  85/3
+
+
